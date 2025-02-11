5553001728d3aa431de36c6b1fd0074cc9fe8282
owenstuckman <ostuckman@outlook.com>
2025-02-11 16:55:18 -0500

    Build to custom domain

--- svelte.config.js
+++ svelte.config.js
@@ -13,7 +13,10 @@ const config = {
 			strict: true
 		}),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/owenstuckman.github.io' : ''
+			base: ''
+		},
+		alias: {
+			$lib: './src/lib'
 		}
 	},
 	extensions: ['.svelte', '.svx', '.md'],
