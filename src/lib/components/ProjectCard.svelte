e467b1d5af431c977e7bde8d3eeb1de3fa4fb6c1
owenstuckman <ostuckman@outlook.com>
2025-02-15 21:52:54 -0500

    Content update for supabase-flutter quickstart
    also fixed how additional images look

--- src/lib/components/ProjectCard.svelte
+++ src/lib/components/ProjectCard.svelte
@@ -184,7 +184,7 @@
 
     .additional-images img {
       width: 100%;
-      height: 220px; /* Increased from 200px */
+      height: 100%;
       object-fit: cover;
       border-radius: 10px;
     }
