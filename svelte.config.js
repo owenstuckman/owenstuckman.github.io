aad8174f2beb122c3104b48bddd198b96fc61de5
owenstuckman <ostuckman@outlook.com>
2025-03-04 15:53:46 -0500

    update styling

--- svelte.config.js
+++ svelte.config.js
@@ -1,33 +1,39 @@
-import adapter from '@sveltejs/adapter-static';
-import { mdsvex } from 'mdsvex';
-import sveltePreprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-vercel'
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+
+import { mdsvex, escapeSvelte } from 'mdsvex'
+import { createHighlighter } from 'shiki'
+import remarkToc from 'remark-toc'
+import rehypeSlug from 'rehype-slug'
+
+/** @type {import('mdsvex').MdsvexOptions} */
+const mdsvexOptions = {
+	extensions: ['.md'],
+	layout: {
+		_: './src/mdsvex.svelte'
+	},
+	highlight: {
+		highlighter: async (code, lang = 'text') => {
+			const highlighter = await createHighlighter({
+				themes: ['poimandres'],
+				langs: ['javascript', 'typescript']
+			})
+			await highlighter.loadLanguage('javascript', 'typescript')
+			const html = escapeSvelte(highlighter.codeToHtml(code, { lang, theme: 'poimandres' }))
+			return `{@html \`${html}\` }`
+		}
+	},
+	remarkPlugins: [[remarkToc, { tight: true }]],
+	rehypePlugins: [rehypeSlug]
+}
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	extensions: ['.svelte', '.md'],
+	preprocess: [vitePreprocess(), mdsvex(mdsvexOptions)],
 	kit: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: '404.html',
-			precompress: false,
-			strict: true
-		}),
-		paths: {
-			base: ''
-		},
-		alias: {
-			$lib: './src/lib'
-		}
-	},
-	extensions: ['.svelte', '.svx', '.md'],
-	preprocess: [
-		sveltePreprocess({
-			//postcss: true,
-		}),
-		mdsvex({
-			extensions: ['.svx', '.md']
-		})
-	]
-};
+		adapter: adapter()
+	}
+}
 
-export default config;
+export default config
