4e81f1d9a9788d5d66c9116e0012fd20eaa95fbb
owenstuckman <ostuckman@outlook.com>
2025-03-10 19:06:00 -0400

    commenting - testing api key

--- src/lib/components/Callout.svelte
+++ src/lib/components/Callout.svelte
@@ -1,13 +1,16 @@
 <script>
+	// Type alias 'Props' represents an object that may contain a 'children' property of type 'Snippet' from Svelte
 	/**
 	 * @typedef {Object} Props
 	 * @property {import('svelte').Snippet} [children]
 	 */
 
+	// Initialize the 'children' variable from the component's props, adhering to the 'Props' type
 	/** @type {Props} */
 	let { children } = $props();
 </script>
 
 <div class="callout">
+	// Conditionally render 'children' if it is defined, utilizing the Svelte render function
 	{@render children?.()}
 </div>
